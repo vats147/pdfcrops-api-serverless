f6a79bb6c644cb6ad21d6610918488771ac82ec8
vats <vatsalmangukiya9003@gmail.com>
2023-08-25 16:23:21 +0530

    chnage ./tmp/ to /tmp

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var fname;
 // Set up multer to handle file uploads
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-         cb(null, './tmp/');
+         cb(null, '/tmp/');
   },
   filename: (req, file, cb) => {
     check++;
@@ -102,7 +102,7 @@ console.log("=================================================");
            // console.log();
            // Save the output PDF file
            fs.writeFileSync(outputPath, await pdfDoc.save());
-           fs.unlink( './tmp/'+fname, (err) => {
+           fs.unlink( '/tmp/'+fname, (err) => {
                   if (err) {
                    
                     console.error(err);
@@ -203,7 +203,7 @@ console.log("=================================================");
      if(req.body.Ecommerce==2)
     {
 
-    cropPDF('./tmp/' + fname,'outputfiledownload.pdf', 170, 467, 255, 353)
+    cropPDF('/tmp/' + fname,'outputfiledownload.pdf', 170, 467, 255, 353)
            .then(() => {
                   console.log("PDF is cropped");
                   // PDF has been cropped
@@ -219,7 +219,7 @@ console.log("=================================================");
     else if(req.body.Ecommerce==3)
     {
           
-           cropPDF('./tmp/' + fname,'outputfiledownload.pdf',  0, 490, 600, 600)
+           cropPDF('/tmp/' + fname,'outputfiledownload.pdf',  0, 490, 600, 600)
            .then(() => {
                   console.log("Meesho is cropped");
                   // PDF has been cropped
@@ -235,7 +235,7 @@ console.log("=================================================");
      else if(req.body.Ecommerce==4)
     {
           
-           cropPDF('./tmp/' + fname,'outputfiledownload.pdf', 25, 220, 545, 300)
+           cropPDF('/tmp/' + fname,'outputfiledownload.pdf', 25, 220, 545, 300)
            .then(() => {
                   console.log("GlowRoad is cropped");
                   // PDF has been cropped
